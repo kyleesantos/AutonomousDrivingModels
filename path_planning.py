bfeb7f03387cf55efd63fa75e3b2011ad06e2445
Kylee Tiger Santos <kyleetigersantos@gmail.com>
2020-03-20 19:28:51 -0700

    Add angle for cars

--- path_planning.py
+++ path_planning.py
@@ -1,12 +1,31 @@
 from track import Track
 from vehicle import Vehicle
 
+vehiclesLeft = []
+vehiclesRight = []
+
 LEFT = 0
 RIGHT = 1
 
+def init_vehicles(side, points):
+  if side == LEFT:
+    vehiclesLeft = [Vehicle(point[0], point[1], point[2]) for point in points]
+  elif side == RIGHT:
+    vehiclesRight = [Vehicle(point[0], point[1], point[2]) for point in points]
+  else:
+    print("Error: Invalid side argument")
+
+
+def init_track():
+  track = Track(vehiclesLeft, vehiclesRight)
+  for vehicle in vehiclesLeft:
+    vehicle.set_path(track.getCircle(LEFT))
+
+  for vehicle in vehiclesRight:
+    vehicle.set_path(track.getCircle(RIGHT))
+
+
 if __name__ == "__main__":
-  vehiclesLeft = [Vehicle(1.0,1.0), Vehicle(2.0,4.0), Vehicle(5.0,3.0)]
-  vehiclesRight = [Vehicle(5.0,1.0), Vehicle(6.0,4.0), Vehicle(9.0,3.0)]
   track = Track(vehiclesLeft, vehiclesRight)
   print(track.getIntersections())
   print(track.getCircle(LEFT))
